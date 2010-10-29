ed28c592fc1553beaceaead27ac2a7d45df26b1f
thattommyhall <thattommyhall@gmail.com>
2010-10-29 22:55:45 +0100

    init

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject primes "1.0.0"
+  :description "FIXME: write"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies [[lein-run "1.0.0"]])
---
+++ src/primes/core.clj
@@ -0,0 +1,1 @@
+(ns primes.core)
