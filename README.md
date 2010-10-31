9b6b3f9f0dcb64038360e65121a3f2bfcf8073ec
thattommyhall <thattommyhall@gmail.com>
2010-10-31 13:04:03 +0000

    made simple-primes and started primes-ieve

--- README.md
+++ README.md
@@ -1,17 +1,14 @@
 # primes
 
-FIXME: write description
+Trying to implement the Genuine Sieve of Eratosthenes from:
+* http://lambda-the-ultimate.org/node/3127
+* http://www.cs.hmc.edu/~oneill/papers/Sieve-JFP.pdf
 
-## Usage
 
-FIXME: write
 
-## Installation
-
-FIXME: write
 
 ## License
 
-Copyright (C) 2010 FIXME
+Copyright (C) 2010 Tom Hall 
 
 Distributed under the Eclipse Public License, the same as Clojure.
--- primes.clj
+++ primes.clj
@@ -5,7 +5,7 @@
        (let [primes-from
 	     (fn primes-from [n [f & r]]
 	       (if (some #(zero? (rem n %))
-		      (take-while #(<= (* % %) n) primes))
+			 (take-while #(<= (* % %) n) primes))
 		 (recur (+ n f) r)
 		 (lazy-seq (cons n (primes-from (+ n f) r)))))
 	     wheel (cycle [2 4 2 4 6 2 6 4 2 4 6 6 2 6 4 2
@@ -15,6 +15,7 @@
 
 (println (take 10 primes))
 
+
 (def primes-simple
      (concat
       [2 3 5 7]
@@ -22,10 +23,37 @@
        (let [primes-from
 	     (fn primes-from [n]
 	       (if (some #(zero? (rem n %))
-			 (take-while #(<= (* % %) n) primes))
+			 (take-while #(<= (* % %) n) primes-simple))
 		 (recur (+ n 1))
 		 (lazy-seq (cons n (primes-from (+ n 1))))))]
-	 (primes-from 11 )))))
+	 (primes-from 11)))))
 
 (println (take 10 primes-simple))
-     
+
+
+
+(def primes-sieve
+     (concat
+      [2]
+      (lazy-seq
+       (let [primes-from
+	     (fn primes-from [n composites]
+	       (if (= n (ffirst composites))
+		 (recur (+ n 1)
+			(merge
+			 (dissoc composites n)
+			 ;need to add code for updated composites map
+			 ())
+			 
+			(lazy-seq
+			 (cons n
+			       (primes-from (+ n 1)
+					    (assoc composites
+					      (* n n)
+					      (concat (composites (* n n)) [n])))))))]
+	     (primes-from 3 (sorted-map 4 [2])))))))
+
+
+
+(println (take 10 primes-sieve))
+
