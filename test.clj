24c13b8dc9db76278bfd190518c6aca795641736
thattommyhall <thattommyhall@gmail.com>
2010-10-31 23:14:26 +0000

    fucking did it!

--- test.clj
+++
@@ -1,6 +0,0 @@
-(def composites (sorted-map 12 [2,3], 14 [7], 15 [5]))
-(def n 12)
-
-(merge-with cons
-						(dissoc composites n)
-						(zipmap (map + (cycle [n]) (composites n)) (composites n)))
