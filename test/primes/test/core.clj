4c972e7869e244741e50ee3cee8c597cb7d8381f
thattommyhall <thattommyhall@gmail.com>
2010-11-01 12:48:56 +0000

    playing with tests

--- test/primes/test/core.clj
+++ test/primes/test/core.clj
@@ -1,23 +1,22 @@
 (ns primes.test.core
   (:use [primes.core] :reload-all)
-  (:use [criterium.core])
-										;(:use [clojure.test])
+  (:require [criterium.core])
+  (:use [clojure.test])
   )
 
-;(deftest checkresult
-;  (is (= (take 1000 primes)
-;	 (take 1000 primes-simple)
-;	 (take 1000 primes-sieve))))
+(deftest checkresult
+  (is (= (take 1000 primes)
+	 (take 1000 primes-simple)
+	 (take 1000 primes-sieve))))
 
+(println "primes lazy mistake")
+(criterium.core/bench (take 10 (drop 100000 primes)) )
 
 (println "primes")
-(time (println (take 5 (drop 1000000 primes)) ))
-
-(println "primes")
-(time (take 10 (drop 1000000 primes)) )
+(criterium.core/bench (println (take 5 (drop 100000 primes)) ))
 
 (println "primes-simple")
-(time (println (take 5 (drop 1000000 primes-simple)) ))
+(criterium.core/bench (println (take 5 (drop 100000 primes-simple)) ))
 
 (println "primes-sieve")
-(time (println (take 5 (drop 1000000 primes-sieve)) ))
+(criterium.core/bench (println (take 5 (drop 100000 primes-sieve)) ))
